202337ddec6218a3f4f30f5fe34d6a575be4e273
e-burch3 <143116447+e-burch3@users.noreply.github.com>
2023-11-30 13:22:22 -0500

    Completed P13B-2

--- LabProblem13B.cpp
+++ LabProblem13B.cpp
@@ -15,25 +15,55 @@ using namespace std;
 
 //prototypes
 bool isLeapYear(int year);
+int daysInMonth(int month, int year);
 
-
+const string MONTHS[] = { "January", "February", "March", "April", "May", "June", "July", "August",
+							"September", "October", "November", "December" };
 int main(void) {
 	int year;
+	int month;
 	while (true) {
-		cout << "Enter a year or Q to quit: ";
+		cout << "Enter a month or Q to quit: ";
+		cin >> month;
 		cin >> year;
 		if (cin.fail())
 			break;
-		if (isLeapYear(year))
-			cout << year << " is a leap year.\n";
-		else
-			cout << year << " is not a leap year.\n";
+		cout << MONTHS[month - 1] << ' ' << year << " has " << daysInMonth(month, year) << " days.\n";
 	}
 }
 
 bool isLeapYear(int year) {
-	if (year % 4 && year % 100)
+	if (year % 4 || year % 400)
 		return false;
 	else
 		return true;
+}
+
+int daysInMonth(int month, int year) {
+	switch (month) {
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		return 31;
+		break;
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		return 30;
+		break;
+	case 2:
+		if (isLeapYear(year))
+			return 29;
+		else
+			return 28;
+		break;
+	default:
+		return NULL;
+		break;
+	}
 }
